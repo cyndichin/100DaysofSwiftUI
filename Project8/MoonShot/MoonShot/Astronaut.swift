d0a95ad9e232e84de6c4c113547eafd2ad989de0
Cyndi Chin <cgc95@cornell.edu>
2021-06-10 20:05:51 -0400

    Update MoonShot Prorogress

---
+++ Project8/MoonShot/MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by cynber on 3/3/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
